b0a10fecb14b42e1f0b743d8b4f883df443e6a84
UTP <jonathanvegagomez15@gmail.com>
2024-02-22 13:31:42 -0600

    Funciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Videojuegos_jon';
+  carrera="TI";
+
+  producto1={
+    nombre:"foto",
+    precio:"32",
+    foto:"./assets/foto.jpg"
+  }
+
+  producto2={
+    nombre:"fotos",
+    precio:"63",
+    foto:"./assets/imagenes/fotos.jpg"
+  }
+
 }
+
+
